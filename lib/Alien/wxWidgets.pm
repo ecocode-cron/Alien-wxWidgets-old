89813b85bbf25d34f0b0f838a7272fea58142da3
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2006-07-09 10:24:32 +0000

    Forgot to commit changes for 0.17: do not test POD for bundled libraries.

--- lib/Alien/wxWidgets.pm
+++ lib/Alien/wxWidgets.pm
@@ -43,7 +43,7 @@ use Module::Pluggable sub_name      => '_list',
                       instantiate   => 'config';
 
 our $AUTOLOAD;
-our $VERSION = '0.16';
+our $VERSION = '0.17';
 our %VALUES;
 our $dont_remap;
 
