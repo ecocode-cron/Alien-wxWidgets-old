75c1fe9fd9ac42a12fc940683bc3e3eafbdee20b
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2006-10-19 19:56:43 +0000

    Use a smart default for the "Do you want to build wxWidgets?" question.

--- lib/Alien/wxWidgets.pm
+++ lib/Alien/wxWidgets.pm
@@ -43,7 +43,7 @@ use Module::Pluggable sub_name      => '_list',
                       instantiate   => 'config';
 
 our $AUTOLOAD;
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 our %VALUES;
 our $dont_remap;
 
