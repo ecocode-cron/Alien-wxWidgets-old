6be7d69cdf783c749ab21fbffc7a1b1338d47abc
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2007-03-16 19:14:36 +0000

    Require Perl 5.006, not 5.6.   Prepare release 0.29.

--- lib/Alien/wxWidgets.pm
+++ lib/Alien/wxWidgets.pm
@@ -43,7 +43,7 @@ use Module::Pluggable sub_name      => '_list',
                       instantiate   => 'config';
 
 our $AUTOLOAD;
-our $VERSION = '0.28';
+our $VERSION = '0.29';
 our %VALUES;
 our $dont_remap;
 
