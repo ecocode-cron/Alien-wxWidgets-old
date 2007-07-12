9817b34b2b6fcf8fffc744a95bbccdce3f857200
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2007-07-12 12:52:13 +0000

    Under Mac, try to propagate flags for Universal binary builds correctly when wxWidgets is build as Universal.

--- inc/My/Build/MacOSX_wx_config.pm
+++ inc/My/Build/MacOSX_wx_config.pm
@@ -8,13 +8,29 @@ sub awx_wx_config_data {
     return $self->{awx_data} if $self->{awx_data};
     my %data = %{$self->SUPER::awx_wx_config_data};
 
-    # MakeMaker does not like the "-framework foo" options
-    $data{libs} =~ s{-framework\s+\w+|-L/usr/local/lib\s}{}g;
+    # MakeMaker does not like some options
+    $data{libs} =~ s{-framework\s+\w+}{}g;
+    $data{libs} =~ s{-isysroot\s+\S+}{}g;
+    $data{libs} =~ s{-L/usr/local/lib\s}{}g;
+
+    $data{libs} =~ s{\s(-arch\s+\w+)}
+                    {$data{linkflags} .= " $1 ";
+                     $data{cxxflags} .= " $1 ";
+                     ' '}eg;
 
     $data{ld} = $data{cxx};
     $data{cxxflags} .= ' -UWX_PRECOMP ';
 
     $self->{awx_data} = \%data;
+}
+
+sub awx_configure {
+    my $self = shift;
+    my %config = $self->SUPER::awx_configure;
+
+    $config{link_flags} .= $self->wx_config( 'linkflags' );
+
+    return %config;
 }
 
 sub awx_build_toolkit { 'mac' }
