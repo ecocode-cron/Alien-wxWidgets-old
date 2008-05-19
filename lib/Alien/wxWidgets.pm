a0a8cb28d5430cbc46eb637c78493c6993ee0bd6
mbarbon <mbarbon@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2008-05-19 21:39:27 +0000

    Starting release 0.37.

--- lib/Alien/wxWidgets.pm
+++ lib/Alien/wxWidgets.pm
@@ -43,7 +43,7 @@ use Module::Pluggable sub_name      => '_list',
                       instantiate   => 'config';
 
 our $AUTOLOAD;
-our $VERSION = '0.36';
+our $VERSION = '0.37';
 our %VALUES;
 our $dont_remap;
 
