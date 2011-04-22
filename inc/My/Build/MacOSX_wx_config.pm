b0b89dd88ad8ccf0cb9dcd522649cb8caa86772d
mdootson <mdootson@2e9104e8-d5c8-4c90-bb67-bfd1792b7337>
2011-04-22 03:28:48 +0000

    Prep CPAN 0.51_01 release

--- inc/My/Build/MacOSX_wx_config.pm
+++ inc/My/Build/MacOSX_wx_config.pm
@@ -43,18 +43,23 @@ sub wxwidgets_configure_extra_flags {
     
     print qq(Standard Mac Flags in use\n);
     
+    my $darwinver = 0;
+    if(`uname -r` =~ /^(\d+)\./) {
+        $darwinver = $1;
+    }
+    
     # on Snow Leopard, force wxWidgets 2.8.x builds to be 32-bit;
     if(    $self->notes( 'build_data' )->{data}{version} =~ /^2.8/
-        && `uname -r` =~ /^10\./
+        && $darwinver >= 10
         && `sysctl hw.cpu64bit_capable` =~ /^hw.cpu64bit_capable: 1/ ) {
         print "Forcing wxWidgets build to 32 bit\n";
         $extra_flags = join ' ', map { qq{$_="-arch i386"} }
                                      qw(CFLAGS CXXFLAGS LDFLAGS
                                         OBJCFLAGS OBJCXXFLAGS);
     }
-    # build fix for 2.9.0 on Snow Leopard
-    if(    `uname -r` =~ /^10\./
-        && $self->notes( 'build_data' )->{data}{version} eq '2.9.0' ) {
+    # build fix for 2.9.x on darwin 10 +
+    if( $darwinver >= 10
+        && $self->notes( 'build_data' )->{data}{version} =~ /^2.9/ ) {
         $extra_flags .= ' --with-macosx-version-min=10.5';
     }
 
--- lib/Alien/wxWidgets.pm
+++ lib/Alien/wxWidgets.pm
@@ -43,7 +43,7 @@ use Module::Pluggable sub_name      => '_list',
                       instantiate   => 'config';
 
 our $AUTOLOAD;
-our $VERSION = '0.51';
+our $VERSION = '0.51_01';
 our %VALUES;
 our $dont_remap;
 
